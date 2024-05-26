63825c597b616fb477c46b965beb6891c3cc3b01
chrisgalanis <chrisgalan2004@gmail.com>
2024-05-27 01:23:50 +0300

    Both Plot in One Graphs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# hotel_booking_data_science
+# Hotel Booking 
+
+### Link to Data Set :  https://www.kaggle.com/datasets/mojtaba142/hotel-booking
+
+### Necessary Libraries before Running  
+    -   pandas : sudo pip3 install pandas #pip3 for python3 version
+    -   matplotlib : sudo pip3 install matplotlib 
+
+### How to run the Programm :
+Open a terminal in the directory with main.py . 
+Write "make" and the programm will run !
--- main.py
+++ main.py
@@ -7,21 +7,24 @@ class Graph:
             self.hotel_name = hotel_name
       
          
-        def plotGraph(self, list, title, xlabel, ylabel):
+        def plotGraph(self, list_city, list_resort , title, xlabel, ylabel):
             print ("---- ",self.hotel_name ,"----")
             print ("\n")
             
             # Plotting the graph
             plt.figure(figsize=(15,5))
-            bars = plt.bar(list.index, list)
+            bar_city = plt.bar(list_city.index, list_city, width=0.4, label='City Hotel')
+            bar_resort = plt.bar(list_resort.index, list_resort, width=0.4, label='Resort Hotel', align='edge')
             
-            for bar in bars:
-                yval = bar.get_height()
-                plt.text(bar.get_x() + bar.get_width()/2, yval, int(yval), va='bottom') 
+            for bars in [bar_city, bar_resort]:
+                for bar in bars:
+                    yval = bar.get_height()
+                    plt.text(bar.get_x() + bar.get_width()/2, yval, int(yval), va='bottom') 
 
-            plt.title(self.hotel_name + ": " + title)
+            plt.title(title)
             plt.xlabel(xlabel)
             plt.ylabel(ylabel)
+            plt.legend()
             plt.show()
             
             print ("\n")
@@ -63,30 +66,87 @@ class Input:
         print ("\n")
         
     
-    def hotel_plots(self, hotel_name):
+    def reservation_per_month_n_season(self, hotel_name):
+        reservations_per_month = {}
+        plot_per_season = {}
+        
+        for name in hotel_name:
+            hotel = self.hotel_booking.loc[self.hotel_booking['hotel'] == name]
+            graph = Graph(name)
+            
+            # Reservations Per Month
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            reservations_per_month[name] = hotel['arrival_date_month'].value_counts()
+            reservations_per_month[name] = reservations_per_month[name].reindex(months)
+            # print("Reservations Per Month: ", reservations_per_month)
+           
+            # Reservation per Season
+            seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+            
+            reservations_per_season = {}
+            j = 0
+            for i in range(2, 11, 3):
+                reservations_per_season[seasons[j]] = reservations_per_month[name].iloc[ i : i+3].sum()
+                j += 1
+                
+            reservations_per_season['Winter'] = reservations_per_month[name].iloc[0:2].sum() + reservations_per_month[name].iloc[11]
+            plot_per_season[name] = pd.Series(reservations_per_season)
+            
+        
+        graph.plotGraph(reservations_per_month['City Hotel'], reservations_per_month['Resort Hotel'], "Reservations Per Month", "Months", "Number of Reservations")
+        graph.plotGraph(plot_per_season['City Hotel'], plot_per_season['Resort Hotel'], "Reservations Per Season", "Seasons", "Number of Reservations")
+        
+    def reservation_per_roomtype(self, hotel_name):
         hotel = self.hotel_booking.loc[self.hotel_booking['hotel'] == hotel_name]
         graph = Graph(hotel_name)
         
-        # Reservations Per Month
-        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        reservations_per_month = hotel['arrival_date_month'].value_counts()
-        reservations_per_month = reservations_per_month.reindex(months)
-        # print("Reservations Per Month: ", reservations_per_month)
-        graph.plotGraph(reservations_per_month, "Reservations Per Month", "Months", "Number of Reservations")
+        # Reservations Per Room Type
+        reservations_per_room_type = hotel['reserved_room_type'].value_counts()
+        reservations_per_room_type = reservations_per_room_type.sort_index()
+        print("Reservations Per Room Type: ", reservations_per_room_type)
+        graph.plotGraph(reservations_per_room_type, "Reservations Per Room Type", "Room Type", "Number of Reservations")
         
-        # Reservation per Season
-        seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+    def reservation_per_visitortype(self, hotel_name):
+        hotel = self.hotel_booking.loc[self.hotel_booking['hotel'] == hotel_name]
+        graph = Graph(hotel_name)
         
-        reservations_per_season = {}
-        j = 0
-        for i in range(2, 11, 3):
-            reservations_per_season[seasons[j]] = reservations_per_month.iloc[ i : i+3].sum()
-            j += 1
+        # Reservations Per Visitor Type
+        alone = hotel.apply(lambda row: row['adults'] == 1 and row['children'] == 0 and row['babies'] == 0, axis=1).sum()
+        print("Alone Visitors: ",alone)
+
+        couple = hotel.apply(lambda row: row['adults'] >= 2 and row['children'] == 0 and row['babies'] == 0, axis=1).sum()
+        print("Couples: ", couple)
+
+        family = hotel.apply(lambda row: row['adults'] >= 0 and (row['children'] >= 1 or row['babies'] >= 1), axis=1).sum()
+        print("Families: ", family)
+
+        notvalid = hotel.apply(lambda row: row['adults'] == 0 and row['children'] == 0 and row['babies'] == 0, axis=1).sum()
+        print("Not Valid: ", notvalid)
+
+        total = alone + couple + family + notvalid
+        print ("Total: ", total , hotel.shape[0])
+
+        x = ["Alone", "Couple", "Family", "Not Valid"]
+        y = [alone, couple, family, notvalid]
+        Series = pd.Series(y, index = x)
+        
+        graph.plotGraph(Series, "Reservations Per Visitor Type", "Visitor Type", "Number of Reservations")
+
+    
+    def reservations_per_year(self, hotel_name):
+        reservations_per_year = {}
+        graph = Graph(hotel_name)
+        for name in hotel_name:
+            hotel = self.hotel_booking.loc[self.hotel_booking['hotel'] == name]
             
-        reservations_per_season['Winter'] = reservations_per_month.iloc[0:2].sum() + reservations_per_month.iloc[11]
-        print("Reservations Per Season: ", reservations_per_season)
+            
+            # Reservations Per Year
+            reservations_per_year[name] = hotel['arrival_date_year'].value_counts()
+            print("Reservations Per Year: ", reservations_per_year)
+            
+        graph.plotGraph(reservations_per_year['City Hotel'], reservations_per_year['Resort Hotel'] , "Reservations Per Year", "Year", "Number of Reservations")
+
         
-        graph.plotGraph(pd.Series(reservations_per_season), "Reservations Per Season", "Seasons", "Number of Reservations")
         
         
 if __name__ == '__main__':
@@ -97,20 +157,38 @@ if __name__ == '__main__':
 
     ## 1. Hotel Statistics ##
     
+    # #City Hotel
+    # input.hotel_statistics('City Hotel')
+    
+    # #Resort Hotel
+    # input.hotel_statistics('Resort Hotel')
+    
+    # ## 2. Plots Reservations Per Month and Per Season ##
+      
+    input.reservation_per_month_n_season(['City Hotel', 'Resort Hotel'])
+    
+    
+    
+    # ## 3. Plots reservation per Room Type ##
+    
+    # #City Hotel
+    # input.reservation_per_roomtype('City Hotel')
+    
+    # #Resort Hotel   
+    # input.reservation_per_roomtype('Resort Hotel')
+    
+    
+    ## 4. Plots reservation per Customer Type ##
+    
     #City Hotel
-    input.hotel_statistics('City Hotel')
+    # input.reservation_per_visitortype('City Hotel')
     
-    #Resort Hotel
-    input.hotel_statistics('Resort Hotel')
-    
-    ## 2. Hotel Plots ##
-    
-    #City Hotel
-    input.hotel_plots('City Hotel')
-    
-    #Resort Hotel   
-    input.hotel_plots('Resort Hotel')
-    
-    
+    # #Resort Hotel   
+    # input.reservation_per_visitortype('Resort Hotel')
+
+    ## 5. Plots reservation per Year ##
+    input.reservations_per_year(['City Hotel', 'Resort Hotel'] )
+
+
     
     
